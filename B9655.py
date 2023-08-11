20eb262e2b3770ebda2d23e5a02a1aa2891d8692
MuJae <phs514525@nate.com>
2023-08-12 02:16:50 +0900

    gogo

---
+++ B9655.py
@@ -0,0 +1,15 @@
+n = int(input())
+if n%3==1:
+    if (n//3)%2==0:
+        print('SK')
+    else:
+        print('CY')
+else:
+    if (n//3)%2==0:
+        print('CY')
+    else:
+        print('SK')
+    
+    
+    
+    
