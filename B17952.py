d7c56fca9a24f348cba30b7014bd3a6f493c063d
Mujae <38150072+Mujae@users.noreply.github.com>
2024-03-01 20:21:59 +0900

    Add files via upload

---
+++ B17952.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+li=[]
+s=0
+for i in range(n):
+    a = list(map(int, input().split()))
+    if len(a)==1:
+        if li:
+            if li[-1][2]==1:
+                s+=li[-1][1]
+                li.pop()
+            else:
+                li[-1][2]-=1
+    else:
+        li.append(a)
+        li[-1][2]-=1
+        if li[-1][2]==0:
+            s+=li[-1][1]
+            li.pop()
+print(s)
+        
