e21cbedc7ad50202ff0875608c3cb320ecb26169
mujae <phs514525@nate.com>
2024-03-23 18:34:40 +0900

    코테준비

--- B1515.py
+++ B1515.py
@@ -6,25 +6,21 @@ c=0#비교변수
 l=len(s)
 i=0
 while i<l:
-    mm=0
     c+=1
-    cs=len(str(c))
-    m=int(s[i:i+min(cs,l-i)])
-        
-    if m==0:
-        mm=1
     k=c
     while True:
-        ks=str(k)
+        sk=str(k)
         check=0
-        for j in range(len(ks)):
-            if ks[j]==s[i]:
-                check+=1
-        if check>0:
-            c=k
+        lk=len(sk)
+        q=0
+        for p in range(lk):
+            if i+q<l:
+                if s[i+q]==sk[p]:
+                    check+=1
+                    q+=1
+        if q>0:
             break
-        else:
-            k+=1
+        k+=1
     c=k
     i+=check
 print(c)
