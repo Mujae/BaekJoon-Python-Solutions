33cf13b35742623acd931606ff14418256951d51
Mujae <38150072+Mujae@users.noreply.github.com>
2025-01-01 12:44:11 +0900

    탑 보기 왼쪽 stack 활용, 오른쪽 매번 탐색 시간초과

--- B22866.py
+++ B22866.py
@@ -5,31 +5,39 @@ input = sys.stdin.readline
 
 N = int(input())
 
-li = list(map(int, input().split()))
+right_que = deque(map(int, input().split()))
+left_que = deque()
 
-m=li[0]
-li2 = [0]*N
-
-for i in range(1, N):
-    if li[i]>m:
-        li2[i]+=1
-        m=li[i]
-
-l = sum(li2)
-
-sta = deque([li[0]])
-
-for i in range(1, N):
-    if sta[-1]>li[i]:
-        sta.append(li[i])
-    elif sta[-1]<li[i]:
-        while sta:
-            if sta[-1]>li[i]:
-                break
-            sta.pop()
-        sta.append(li[i])
-    l2 = len(sta)
-    if l2 + l -1== 0:
+for i in range(N):
+    c=0
+    mr=0
+    while left_que:
+        if right_que[0]>=left_que[-1][0]:
+            left_que.pop()
+        else:
+            break
+        
+    left_que.append((right_que.popleft(),i))
+    l=left_que[-1][0]
+    
+    for j in range(len(right_que)):
+        if right_que[j]>l:
+            l=right_que[j]
+            c+=1
+            if mr==0:
+                mr=i+j+1
+                
+    if len(left_que) - 1 + c == 0:
         print(0)
     else:
-        print(li[i], len(sta)+l-1)
+        if len(left_que)==1:#첫번째 or 뒤에 같은 것만 많을 때)
+            print(c, mr+1)
+        elif i==N-1:
+            print(len(left_que)-1,left_que[-1][1]+1)
+        else:
+            print(len(left_que)-1+c, end = " ")
+            if i-left_que[-2][1]<=mr-i:
+                print(left_que[-2][1]+1)
+            else:
+                print(mr+1)
+                
