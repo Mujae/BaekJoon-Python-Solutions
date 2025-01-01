6de000f42b0ba5d5d264dceeaed3a2987b8d2d32
Mujae <38150072+Mujae@users.noreply.github.com>
2025-01-02 01:51:58 +0900

    오른쪽, 왼쪽 둘 다 stack으로 시간관리

--- B22866.py
+++ B22866.py
@@ -1,43 +1,60 @@
 import sys
 from collections import deque
 
+
+#왼쪽은 stack, 오른쪽은 매번 탐색하는 코드가 더 깔끔해보였는데 시간초과.
+#아래처럼 메모리는 많이 잡아먹어도 그냥 두 번 돌리니깐 통과. 그냥 구현문제 아닌가?
+
 input = sys.stdin.readline
 
 N = int(input())
-
-right_que = deque(map(int, input().split()))
+li = list(map(int, input().split()))
+right_que = deque()
 left_que = deque()
+save_li = [[] for _ in range(N)]
 
+#원래라면 함수 만들어서 이쁘게 관리할텐데.. 다음기회에
+#left
 for i in range(N):
-    c=0
-    mr=0
     while left_que:
-        if right_que[0]>=left_que[-1][0]:
+        if li[i]>=left_que[-1][0]:
             left_que.pop()
         else:
             break
+    left_que.append((li[i],i))
+    if len(left_que) > 1:
+        save_li[i].append(len(left_que)-1)
+        save_li[i].append(left_que[-2][1])
+    else:
+        save_li[i].append(0)
+        save_li[i].append(-1)
+
+#right
+for i in range(N-1,-1,-1):
+    while right_que:
+        if li[i]>=right_que[-1][0]:
+            right_que.pop()
+        else:
+            break
+    right_que.append((li[i],i))
+
+    if len(right_que) > 1:
+        save_li[i].append(len(right_que)-1)
+        save_li[i].append(right_que[-2][1])
+    else:
+        save_li[i].append(0)
+        save_li[i].append(-1)
         
-    left_que.append((right_que.popleft(),i))
-    l=left_que[-1][0]
-    
-    for j in range(len(right_que)):
-        if right_que[j]>l:
-            l=right_que[j]
-            c+=1
-            if mr==0:
-                mr=i+j+1
-                
-    if len(left_que) - 1 + c == 0:
+for i in range(N):
+    if save_li[i][0]+save_li[i][2]==0:
         print(0)
     else:
-        if len(left_que)==1:#첫번째 or 뒤에 같은 것만 많을 때)
-            print(c, mr+1)
-        elif i==N-1:
-            print(len(left_que)-1,left_que[-1][1]+1)
+        if save_li[i][1]==-1:
+            print(save_li[i][2], save_li[i][3]+1)
+        elif save_li[i][3]==-1:
+            print(save_li[i][0], save_li[i][1]+1)
+        elif i-save_li[i][1] <= save_li[i][3]-i:
+            print(save_li[i][0]+save_li[i][2], save_li[i][1]+1)
         else:
-            print(len(left_que)-1+c, end = " ")
-            if i-left_que[-2][1]<=mr-i:
-                print(left_que[-2][1]+1)
-            else:
-                print(mr+1)
-                
+            print(save_li[i][0]+save_li[i][2], save_li[i][3]+1)
+            
