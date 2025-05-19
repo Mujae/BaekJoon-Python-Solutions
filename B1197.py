543b96381de61d7b4f376fc67cca73db009d4fd6
Mujae <38150072+Mujae@users.noreply.github.com>
2025-05-19 12:36:57 +0900

    최소스패닝트리

--- B1197.py
+++ B1197.py
@@ -1,10 +1,10 @@
 import sys
-
+sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 
 # 간선들을 가중치로 정렬
 # 간선을 값이 적은 것부터 추가
-# 추가할 때 정점을 고려해서 추가하는디 집합하나를 만들어놓고 추가
+# 추가할 때 정점을 고려해서 추가하는디 집합 하나를 만들어놓고 추가
 # 만약 추가하는 정점 두 개가 둘 다 집합에 속해 있으면 그건 사이클을 만드니 추방
 n, m  = map(int, input().split())
 
@@ -15,6 +15,7 @@ for i in range(m):
     li.append(list(map(int, input().split())))
 li = sorted(li, key = lambda x:-x[2])
 parent = [i for i in range(n+1)]
+
 def get_parent(x):
     if parent[x]==x:
         return x
@@ -29,6 +30,7 @@ def union_parent(p, q):
         parent[p] = q
     else:
         parent[q] = p
+        
 def same_parent(p, q):
     return get_parent(p) == get_parent(q)
 
